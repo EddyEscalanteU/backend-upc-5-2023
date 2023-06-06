f8621c80c8f64c2214a405ed897cd0320d434d42
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-06-05 21:28:46 -0400

    create project

---
+++ backend-upc-5-2023/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace backend_upc_5_2023
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
