541fb6e946323eeb00c0f94e0bd82d06155fd0b0
EddyEscalanteU <eddy.escalante.u@hotmail.com>
2023-06-07 19:20:26 -0400

    Cambios Miercoles 07/06/2023

---
+++ backend-upc-5-2023/Controllers/UsuariosController.cs
@@ -0,0 +1,35 @@
+﻿using backend_upc_5_2023.Dominio;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace backend_upc_5_2023.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        readonly IConfiguration _configuration;
+        readonly string connectionString;
+        public UsuariosController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString =
+            _configuration["SqlConnectionString:DefaultConnection"];
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var sql = "select * from Usuarios";
+            var listUsuarios = new List<Usuarios>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                DefaultTypeMap.MatchNamesWithUnderscores = true;
+                listUsuarios = connection.Query<Usuarios>(sql).ToList();
+            }
+            return StatusCode(200, listUsuarios);
+        }
+    }
+}
---
+++ backend-upc-5-2023/Dominio/Usuarios.cs
@@ -0,0 +1,11 @@
+﻿namespace backend_upc_5_2023.Dominio
+{
+    public class Usuarios
+    {
+
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
